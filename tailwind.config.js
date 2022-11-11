48e2d99eff5afe83ada26c119cce456c8524bae4
qocksgh <123qocksgh@naver.com>
2022-11-11 17:49:43 +0900

    22.11.11

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      "sans": ["Poppins", "Arial", "sans-serif"]
+    },
     extend: {},
   },
   plugins: [],
